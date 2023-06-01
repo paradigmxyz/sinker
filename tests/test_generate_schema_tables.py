752171fc8b033b0791c7228fbca5e99cd2c4d476
Loren Siebert <loren@paradigm.xyz>
2023-06-01 11:19:15 -0700

    Fix handling of function calls in materialized view queries (#4)

---
+++ tests/test_generate_schema_tables.py
@@ -0,0 +1,12 @@
+from sinker.utils import generate_schema_tables
+
+
+def test_generate_schema_tables():
+    view_select_query = """select id,
+       json_build_object(
+               'name', "name",
+               'emailDomains',(select array_agg(split_part(email, '@', 2)) FROM unnest(emails) as email),
+               ) as "person"
+        from "person"
+        """
+    assert list(generate_schema_tables(view_select_query)) == ["person"]
