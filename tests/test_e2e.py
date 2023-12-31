c3aa59355f2a03077660b85cdea12da644c00c3e
hkey <81494947+hkey0@users.noreply.github.com>
2023-12-31 04:08:09 +0300

    Made more readable (pep8) (#10)
    * made more readable
    
    * made more readable
    
    * made more readable
    
    * made more readable

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -34,20 +34,25 @@ def wait_for_pg_es():
 
 def test_exception_propagation_during_refresh(wait_for_pg_es):
     runner = Runner()
-    # Update a record in Postgres but remove the materialized view so it errors when we iterate
+    # Update a record in Postgres but remove the materialized view so it
+    # errors when we iterate
     conn = psycopg.connect(autocommit=True)
     cur = conn.cursor()
     cur.execute("update public.person set name = 'Uh oh' where id = 'p-1'")
     cur.execute("drop materialized view sinker.person_mv")
-    # make sure the exception gets raised from the thread versus silently consuming it
+    # make sure the exception gets raised from the thread versus silently
+    # consuming it
     with pytest.raises(psycopg.errors.UndefinedTable):
         runner.iterate()
 
 
 def test_exception_propagation_during_setup(wait_for_pg_es, mocker):
-    # mock es.get_client() to raise an exception in the thread that sets up the sinker
-    mocker.patch("sinker.sinker.get_client", side_effect=elasticsearch.exceptions.ConnectionError('Boom!'))
-    # make sure the exception gets raised from the thread versus silently consuming it
+    # mock es.get_client() to raise an exception in the thread that sets up
+    # the sinker
+    mocker.patch("sinker.sinker.get_client",
+                 side_effect=elasticsearch.exceptions.ConnectionError('Boom!'))
+    # make sure the exception gets raised from the thread versus silently
+    # consuming it
     with pytest.raises(elasticsearch.exceptions.ConnectionError):
         Runner()
 
@@ -92,12 +97,14 @@ def test_end_to_end(wait_for_pg_es):
     assert doc["_source"] == expected
 
     # Update a record in Postgres
-    psycopg.connect(autocommit=True).execute("update public.person set name = 'Jane' where id = 'p-1'")
+    psycopg.connect(autocommit=True).execute(
+        "update public.person set name = 'Jane' where id = 'p-1'")
     runner.iterate()
     # Verify the update made it into the people index
     doc = es.get(index="people", id="p-1")
     assert doc["_source"]["name"] == "Jane"
-    # Verify the update made it into the courses index through person->student->enrollment->course.
+    # Verify the update made it into the courses index through
+    # person->student->enrollment->course.
     doc = es.get(index="courses", id="c-1")
     expected = {
         "name": "Reth",
@@ -137,7 +144,8 @@ def test_end_to_end(wait_for_pg_es):
     # Verify the doc got deleted from Elasticsearch
     with pytest.raises(elasticsearch.NotFoundError):
         es.get(index="people", id="p-1")
-    # Verify the doc got deleted from Elasticsearch through person->student->enrollment->course.
+    # Verify the doc got deleted from Elasticsearch through
+    # person->student->enrollment->course.
     doc = es.get(index="courses", id="c-1")
     expected = {
         "name": "Reth",
